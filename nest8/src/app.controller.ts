0cb0cb05e3a8a86028f9738a01f1206c4a36c17a
Raido Kaldma <raido.kaldma@ria.ee>
2021-11-16 11:17:01 +0200

    Initial commit

---
+++ nest8/src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    throw new Error('I want to log this error');
+  }
+}
